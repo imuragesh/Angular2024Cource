cbaab340e521516dd57dbee9e1db5dc933a51226
muragesh.s <Star@123>
2024-02-18 13:31:08 +0530

    first angular app commit and along with that how we can  use the declered string from .ts file in the .html file

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title='Mypractice App'
+  name = 'This is my first practice';
+  myName:string="Muragesh sidaray hipparagi";
+}
